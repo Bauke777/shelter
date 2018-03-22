bc6310c7d7a69b04613b095c5d58aa147ee4f247
Bauke Posthuma <baukeposthuma@gmail.com>
2018-03-22 16:09:12 +0100

    added DELETE /:id

--- server/index.js
+++ server/index.js
@@ -16,17 +16,18 @@ module.exports = express()
   .get('/:id', get)
   // .put('/:id', set)
   // .patch('/:id', change)
-  // .delete('/:id', remove)
+  .delete('/:id', remove)
   .listen(1902)
 
 function all(req, res) {
 
     var result = {errors: [], data: db.all()}
-    
+
     res.format({
       json: () => res.json(result),
       html: () => res.render('list.ejs', Object.assign({}, result, helpers))
     })
+
 }
 
 function get(req, res) {
@@ -44,6 +45,30 @@ function get(req, res) {
         }
         // If id is not in the database give
         else {
+            var result = {errors: [{id: 404, title: '404', detail: 'Not Found'}]}
+            res.status(404).render('error.ejs', Object.assign({}, result, helpers))
+        }
+    }
+    // Handle invalid identifiers like ('/-')
+    catch(err) {
+        var result = {errors: [{id: 400, title: '400', detail: 'Bad request'}]}
+        res.status(400).render('error.ejs', Object.assign({}, result, helpers))
+        return
+    }
+
+}
+
+function remove(req, res) {
+
+    var id = req.params.id
+
+    try {
+        // If id is availible in the database, render as JSON or HTML
+        if (db.has(id)) {
+            db.remove(id)
+            var result = {errors: [{id: 204, title: '204', detail: 'No content'}]}
+            res.json(result)
+        } else {
             var result = {errors: [{id: 404, title: '404', detail: 'Not Found'}]}
             res.status(404).render('error.ejs', Object.assign({}, result, helpers))
         }
